00660c68d5bfe467de29918fe772e513e78c68a1
nayani <yapancs@gmail.com>
2024-04-29 09:27:42 -0400

    change green color

--- color.js
+++ color.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousemove' , function(e)
     {
-const r = Math.round((e.pageX * 255) / window.innerWidth);
+const g = Math.round((e.pageX * 255) / window.innerWidth);
 const b = Math.round((e.pageY * 255) / window.innerHeight);
-const color = `rgb(${r} , 0 , ${b})`;
+const color = `rgb( 0 , ${g} , ${b})`;
 document.body.style.backgroundColor = color;
 console.log(color);
     }
